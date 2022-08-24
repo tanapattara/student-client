2457c7a3ad224bed8cb18f3da3def5fc9a189cd1
Tanapattara Wongkhamchan <tanapattara@kku.ac.th>
2022-08-24 11:49:44 +0700

    init project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { StudentComponent } from './components/student/student.component';
+import { StudentAddComponent } from './components/student-add/student-add.component';
+import { StudentDetailComponent } from './components/student-detail/student-detail.component';
+import { StudentEditComponent } from './components/student-edit/student-edit.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    StudentComponent,
+    StudentAddComponent,
+    StudentDetailComponent,
+    StudentEditComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    BrowserAnimationsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
